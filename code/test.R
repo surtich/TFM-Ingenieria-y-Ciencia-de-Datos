1dd0b083a975aeb41c9d6c7550606c43177794c3
Javier Pérez <surtich@gmail.com>
2023-03-27 01:49:13 +0200

    Añadido period.qmd

--- code/test.R
+++ code/test.R
@@ -1,14 +1,34 @@
-library(cluster)
+data <- read.table("code/Envir.dat", header = TRUE)
+t <- as_tibble(data) %>%
+    mutate(question = factor(question, labels = c("Drive Less", "Recycle")), y = factor(y, labels = c("Always", "Often", "Sometimes", "Never"))) %>%
+    pivot_wider(
+        names_from = question, values_from =
+            y
+    ) %>%
+    xtabs(~
+        Recycle + `Drive Less`, data = .)
 
-distancias <- df %>%
-    xtabs(~ Question + Response, data = .) %>%
-    daisy(x = ., metric = "euclidean")
 
-# Realizar un clustering con el algoritmo de Ward
-cluster <- agnes(distancias, method = "average", diss = TRUE)
+# Definimos las dimensiones de la matriz
+n <- nrow(t)
+m <- n * (n - 1) / 2
+# Creamos la matriz inicial con todos los valores en cero
+dt <- data.frame()
 
-# Asignar las preguntas a grupos
-grupos <- cutree(cluster, k = 3)
+for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+        r <- matrix(0, nrow = 1, ncol = n + 3)
+        r[1, i] <- 1
+        r[1, j] <- -1
+        r[1, n + 1] <- j - i
+        r[1, n + 2] <- t[i, j]
+        r[1, n + 3] <- t[j, i]
+        dt <- rbind(dt, r)
+    }
+}
 
-# Crear un nuevo data frame con la columna Question y su grupo correspondiente
-(preguntas_grupo <- data.frame(Question = levels(df$Question), Grupo = grupos))
+colnames(dt) <- c(row.names(t), "x", "nij", "nji")
+
+
+fit <- glm(nij / (nij + nji) ~ -1 + x, family = binomial, weights = nij + nji, data = dt)
+summary(fit)
